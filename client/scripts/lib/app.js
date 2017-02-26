b59e5614a7bcf2c5bcb3a222f4f33920bf4c71eb
Tecnologer <rdominguez@tecnologer.net>
2017-02-26 01:46:20 -0700

    First version

--- client/scripts/lib/app.js
+++ client/scripts/lib/app.js
@@ -1,17 +1,27 @@
 // Libs
-import "angular-animate";
-import "angular-meteor";
-import "angular-sanitize";
-import "angular-ui-router";
-import "ionic-scripts";
-import Angular from "angular";
-import { Meteor } from "meteor/meteor";
+import 'angular-animate';
+import 'angular-meteor';
+import 'angular-sanitize';
+import 'angular-ui-router';
+import 'ionic-scripts';
+import Angular from 'angular';
+import Loader from 'angular-ecmascript/module-loader';
+import { Meteor } from 'meteor/meteor';
 
 // Modules
+import ChatsCtrl from '../controllers/chats.controller';
+import CalendarFilter from '../filters/calendar.filter';
+import RoutesConfig from '../routes';
+
 const App = "Whatsapp";
 
 // App
-Angular.module(app,["angular-meteor","ionic"]);
+Angular.module(App,["angular-meteor","ionic"]);
+
+new Loader(App)
+	.load(ChatsCtrl)
+	.load(CalendarFilter)
+	.load(RoutesConfig);
 
 if(Meteor.isCordova){
 	Angular.element(document).on("deviceready", onReady);
